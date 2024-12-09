db1cc5c60b849d004da871b5fe9926d964aa19e6
Abin Antony <abin.antony@aot-technologies.com>
2024-12-09 12:03:43 -0800

    #3598 updated code for comments, removing linux executables

--- computingservices/documentextractservices/azuredocextractservice/src/messageprocessor.go
+++ computingservices/documentextractservices/azuredocextractservice/src/messageprocessor.go
@@ -27,14 +27,7 @@ func main() {
 	// Print each message
 	for _, message := range dequeuedmessages {
 		fmt.Printf("Received message: %+v\n", message)
-		// jsonstr := `{
-		// 	"urlSource": "` + message.S3URI + `"
-		// }`
-		// fmt.Printf("json url str formated %s\n", jsonstr)
-		//var jsonStrbytes = []byte(jsonstr)
-		//var s3url = s3services.GetFilefroms3("ORIGINALPDF.pdf", "test123")
-		// fmt.Printf("MinistryRequestId: %s, RequestNumber: %s, Host: %s\n", message.MinistryRequestId,
-		// 	message.RequestNumber, message.DivisionName)
+
 		parsedURL, err := url.Parse(message.S3Uri)
 		if err != nil {
 			fmt.Printf("Error parsing URL: %v\n", err)
